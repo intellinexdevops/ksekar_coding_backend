a6dcc5c29f13c490e27c05f3c339352c645543f5
Chenter <chenterphai61@gmail.com>
2025-05-27 14:19:16 +0700

    Setup Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthController } from './controllers/auth/auth.controller';
+import { AuthService } from './services/auth/auth.service';
+import { UsersModule } from './modules/users/users.module';
+import { UsersService } from './services/users/users.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService, UsersService],
 })
 export class AppModule {}
